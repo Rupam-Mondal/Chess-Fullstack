f4c58ec4e2fca0ec7e8bfc10282fe0782c2c2fe0
Rupam <rupammondal564@gmail.com>
2025-02-04 20:25:11 +0530

    Pieces move logic added

--- Backend/src/Controller/Player.js
+++ Backend/src/Controller/Player.js
@@ -24,7 +24,24 @@ function ChessplayerLogic(socket, io, chess, players, currPlayer){
 }
 
 function move(socket, io, chess, players, currPlayer){
-    
+    socket.on('move' , (move) => {
+        try {
+            if(chess.turn() == 'w' && socket.id != players.white) return;
+            if(chess.turn() == 'b' && socket.id != players.black) return;
+
+            const res = chess.move(move);
+            if(res){
+                currPlayer = chess.turn();
+                io.emit("move" , move);
+                io.emit("boardState" , chess.fen());
+            }
+            else{
+                console.log("Wrong move");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    })
 }
 
 export default ChessplayerLogic;
